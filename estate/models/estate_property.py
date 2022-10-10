f7a83af14a25dd0d7c91e154e771d4f090eb0bfd
Thibault Betremieux <thibault.betremieux@gmail.com>
2022-10-10 09:37:20 +0000

    test access rights

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,12 +1,29 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
 
-from odoo import fields, models
+from odoo import models, fields, api, tools
+from odoo.exceptions import UserError, ValidationError
+from dateutil.relativedelta import relativedelta
+
+_logger = logging.getLogger(__name__)
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description = "Some estate information"
+    _description = "Estate properties"
+    _order = "id desc"
 
-    title = fields.Char('Plan Name', required=True, translate=True)
-    property_type = fields.Char('Property Type', required=True, translate=True)
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.now() + relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(string='Garden Orientation',
+                                          selection=[('north', 'North'), ('sout', 'South'), ('west', 'West'),
+                                                     ('east', 'East')])
